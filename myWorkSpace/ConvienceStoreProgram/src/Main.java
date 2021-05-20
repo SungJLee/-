e9a1af40a9f12ddfdd2325cd23fba7f9fee7f32b
gustjrdl99 <gustjrdl99@gmail.com>
2021-05-20 11:11:14 +0900

    틀 추가한 버전

--- myWorkSpace/ConvienceStoreProgram/src/Main.java
+++ myWorkSpace/ConvienceStoreProgram/src/Main.java
@@ -1,9 +1,52 @@
+import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+  public static void main(String[] args) {
+		
+		boolean swi = true;
+		
+    // 목록
+		while(swi){
+			System.out.println("1.");
+			System.out.println("2.");
+      System.out.println("3.");
+      System.out.println("4.");
+      System.out.println("5.");
+      System.out.println("6.");
+			System.out.println("7. 나가기");
+			
+      
+			Scanner scan = new Scanner(System.in); 
+			System.out.print("> ");
+			int choice = scan.nextInt();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+      switch(choice){
+				case 1:
+		    break;
 
-	}
+        case 2: 
+        break;
 
+				case 3: 
+				break;
+
+				case 4: 
+				break;
+
+				case 5 : 
+				break;
+
+				case 6 : 
+				break;
+
+        case 7 : // 나가기
+					swi = false;
+				break;
+				default :
+					System.out.println("잘못 입력하셨습니다.");
+				break;
+			}
+    }
+  }
 }
